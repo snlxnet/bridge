281ff4511963f99bde35e29d5e336dd6c6aee064
Alex <alexanderthestudent@yandex.ru>
2026-02-17 20:11:40 +0300

    fix: stop ignoring the ru- pages

--- src/main.ts
+++ src/main.ts
@@ -271,14 +271,6 @@ export default class Bridge extends Plugin {
 								.filter(
 									(entry) => entry.from.path === file.path,
 								)
-								.filter(
-									(entry) =>
-										!entry.for.path.startsWith("ru-"),
-								)
-								.filter(
-									(entry) =>
-										!entry.from.path.startsWith("ru-"),
-								)
 								.map((entry) => entry.for);
 
 							[file, ...allowed].map((file) => {
@@ -508,14 +500,10 @@ export default class Bridge extends Plugin {
 		const root = document.createElement("body");
 		let backLinks = linkTree
 			.filter((entry) => entry.for.path === note.file.path)
-			.filter((entry) => !entry.for.path.startsWith("ru-"))
-			.filter((entry) => !entry.from.path.startsWith("ru-"))
 			.map((entry) => entry.from.basename);
 		backLinks = [...new Set(backLinks)];
 		let forwardLinks = linkTree
 			.filter((entry) => entry.from.path === note.file.path)
-			.filter((entry) => !entry.for.path.startsWith("ru-"))
-			.filter((entry) => !entry.from.path.startsWith("ru-"))
 			.map((entry) => entry.for.basename);
 		forwardLinks = [...new Set(forwardLinks)];
 
